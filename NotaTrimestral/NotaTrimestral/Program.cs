cf573ab6201729f0a2f3b60e226e7d843b36d308
MatheusPaiva <mafbpaiva@gmail.com>
2025-03-06 19:08:01 -0300

    Added new exercise NotaTrimestral

---
+++ NotaTrimestral/NotaTrimestral/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaTrimestral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno all = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            all.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+
+            String[] Vet = Console.ReadLine().Split(' ');
+            all.Nota1 = double.Parse(Vet[0], CultureInfo.InvariantCulture);
+            all.Nota2 = double.Parse(Vet[1], CultureInfo.InvariantCulture);
+            all.Nota3 = double.Parse(Vet[2], CultureInfo.InvariantCulture);
+                     
+
+            if (all.Aprovado())
+            {
+                Console.WriteLine("Nota Final = " + all.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Nota Final = " + all.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Fataram " + all.Resto().ToString("F2", CultureInfo.InvariantCulture) + " Pontos");
+            }
+        }
+    }
+}
---
+++ NotaTrimestral/NotaTrimestral/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("NotaTrimestral")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NotaTrimestral")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("ecde0ec5-0c45-4ea6-9b73-693b235ead53")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
