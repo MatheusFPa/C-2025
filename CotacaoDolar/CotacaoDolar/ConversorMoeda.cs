a92133e6bab2de3ae50fa9d987a599cb41308458
MatheusPaiva <mafbpaiva@gmail.com>
2025-03-07 20:04:23 -0300

    Added Construtor & Sobrecarga

---
+++ CotacaoDolar/CotacaoDolar/ConversorMoeda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CotacaoDolar
+{
+    class ConversorMoeda
+    {
+        public static double Iof = 6.0;
+        
+        public static double DolarEmReal(double quant, double cotacao)
+        {
+            double total = quant * cotacao;
+
+            return total + total * Iof / 100.00;
+        }
+
+      
+    }
+}
---
+++ CotacaoDolar/CotacaoDolar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotacaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dola? ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolare voce vai comprar? ");
+            double qtdDolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double result = ConversorMoeda.DolarEmReal(qtdDolar, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais: " + result);
+        }
+    }
+}
