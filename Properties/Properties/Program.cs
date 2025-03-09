37c784b518faffea95386d8ea9a3bf7536d0286c
MatheusPaiva <mafbpaiva@gmail.com>
2025-03-08 22:50:09 -0300

    Properties

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "Tv 4k";
+
+            Console.WriteLine( p.Nome);
+            Console.WriteLine( p.Preco);
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
